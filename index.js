4e8aa09e2d1d20f4232b5d1813ffe5842cbe2df5
JorgeRa199 <jorgearturo99@live.com.mx>
2020-08-24 14:29:37 -0500

    Icono agregado

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function initMap() {
+
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+
     var coordenadas = {
         lat: 21.152821,
         lng: -101.711612
@@ -7,5 +10,5 @@ function initMap() {
         center: coordenadas,
         zoom: 15
     });
-    var mark = new google.maps.Marker({ position: coordenadas, map });
+    var mark = new google.maps.Marker({ position: coordenadas, map, icon : iconBase + 'parking_lot_maps.png' });
 }
