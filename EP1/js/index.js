791bdb5103530ffe9cbf019f22bbb9ee97d219df
JorgeRa199 <jorgearturo99@live.com.mx>
2020-09-14 15:35:58 -0500

    Segundo mapa aregado

--- EP1/js/index.js
+++ EP1/js/index.js
@@ -1,39 +1,74 @@
 let currentCoordinates = [];
 let latestPolygon = null;
 let addNewPolygon = true;
+let secondMapCoordinates = [];
+let leafletMap2;
+let tiles = [
+  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+];
 
-const leafletMap = L.map('leaflet-map').setView([CENTER_LAT, CENTER_LNG], ZOOM_LEVEL);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
+const leafletMap = L.map("map1").setView([CENTER_LAT, CENTER_LNG], ZOOM_LEVEL);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 18,
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: "mapbox/streets-v11",
+  tileSize: 512,
+  zoomOffset: -1,
 }).addTo(leafletMap);
 
 function onMapClick(e) {
-    if (addNewPolygon) {
-        latestPolygon = null;
-        currentCoordinates = [];
-        addNewPolygon = false;
+  if (addNewPolygon) {
+    latestPolygon = null;
+    currentCoordinates = [];
+    addNewPolygon = false;
+  }
+
+  currentCoordinates.push([e.latlng.lat, e.latlng.lng]);
+  L.marker([e.latlng.lat, e.latlng.lng]).addTo(leafletMap);
+  if (currentCoordinates.length >= MIN_MARKERS) {
+    if (latestPolygon) {
+      leafletMap.removeLayer(latestPolygon);
     }
 
-    currentCoordinates.push([e.latlng.lat, e.latlng.lng]);
-    L.marker([e.latlng.lat, e.latlng.lng]).addTo(leafletMap);
-    if (currentCoordinates.length >= MIN_MARKERS) {
-        if (latestPolygon) {
-            leafletMap.removeLayer(latestPolygon);
-        }
+    latestPolygon = L.polygon(currentCoordinates).addTo(leafletMap);
+    secondMapCoordinates.push.apply(secondMapCoordinates, currentCoordinates);
+    console.log(secondMapCoordinates);
+    //currentCoordinates = [];
 
-        latestPolygon = L.polygon(currentCoordinates).addTo(leafletMap);
+    //SE AGREGA TODO AL SEGUNDO MAPA
+    
+    if(leafletMap2 !== undefined) {
+        leafletMap2.off();
+        leafletMap2.remove();
     }
+    leafletMap2 = L.map("map2").setView(
+      [CENTER_LAT, CENTER_LNG],
+      ZOOM_LEVEL
+    );
+    L.tileLayer( tiles[(Math.floor((Math.random() * 3) + 0))], {
+      maxZoom: 18,
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+    }).addTo(leafletMap2);
+    for (let i = 0; i < secondMapCoordinates.length; i++) {
+        L.marker([secondMapCoordinates[i][0],secondMapCoordinates[i][1]]).addTo(leafletMap2);
+    }
+  }
 }
 
 function onContextMenu(e) {
-    addNewPolygon = true;
+  addNewPolygon = true;
 }
 
-leafletMap.on('click', onMapClick);
-leafletMap.on('contextmenu', onContextMenu);
+leafletMap.on("click", onMapClick);
+leafletMap.on("contextmenu", onContextMenu);
