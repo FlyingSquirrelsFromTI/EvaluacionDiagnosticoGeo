51b72c9c9dba4e7a517ecb98703e1b1d997a91ff
LuisZanella <luiscontreraszane@gmail.com>
2020-09-14 15:52:51 -0500

    Alert metida

--- EP1/js/constants.js
+++ EP1/js/constants.js
@@ -1,4 +1,4 @@
 const CENTER_LAT = 52.3826;
 const CENTER_LNG = 4.8994;
 const ZOOM_LEVEL = 9;
-const MIN_MARKERS = 11;
+const MIN_MARKERS = 3;
--- EP1/js/index.js
+++ EP1/js/index.js
@@ -4,71 +4,88 @@ let addNewPolygon = true;
 let secondMapCoordinates = [];
 let leafletMap2;
 let tiles = [
-  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
 ];
 
 const leafletMap = L.map("map1").setView([CENTER_LAT, CENTER_LNG], ZOOM_LEVEL);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 18,
-  attribution:
-    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  id: "mapbox/streets-v11",
-  tileSize: 512,
-  zoomOffset: -1,
-}).addTo(leafletMap);
-
-function onMapClick(e) {
-  if (addNewPolygon) {
-    latestPolygon = null;
-    currentCoordinates = [];
-    addNewPolygon = false;
-  }
-
-  currentCoordinates.push([e.latlng.lat, e.latlng.lng]);
-  L.marker([e.latlng.lat, e.latlng.lng]).addTo(leafletMap);
-  if (currentCoordinates.length >= MIN_MARKERS) {
-    if (latestPolygon) {
-      leafletMap.removeLayer(latestPolygon);
-    }
-
-    latestPolygon = L.polygon(currentCoordinates).addTo(leafletMap);
-    secondMapCoordinates.push.apply(secondMapCoordinates, currentCoordinates);
-    console.log(secondMapCoordinates);
-    //currentCoordinates = [];
-
-    //SE AGREGA TODO AL SEGUNDO MAPA
-    
-    if(leafletMap2 !== undefined) {
-        leafletMap2.off();
-        leafletMap2.remove();
-    }
-    leafletMap2 = L.map("map2").setView(
-      [CENTER_LAT, CENTER_LNG],
-      ZOOM_LEVEL
-    );
-    L.tileLayer( tiles[(Math.floor((Math.random() * 3) + 0))], {
-      maxZoom: 18,
-      attribution:
+    maxZoom: 18,
+    attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      id: "mapbox/streets-v11",
-      tileSize: 512,
-      zoomOffset: -1,
-    }).addTo(leafletMap2);
-    for (let i = 0; i < secondMapCoordinates.length; i++) {
-        L.marker([secondMapCoordinates[i][0],secondMapCoordinates[i][1]]).addTo(leafletMap2);
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+}).addTo(leafletMap);
+
+function onMapClick(e) {
+    if (addNewPolygon) {
+        latestPolygon = null;
+        currentCoordinates = [];
+        addNewPolygon = false;
     }
-  }
+
+    currentCoordinates.push([e.latlng.lat, e.latlng.lng]);
+    L.marker([e.latlng.lat, e.latlng.lng]).addTo(leafletMap);
+
+    if (currentCoordinates.length >= MIN_MARKERS) {
+
+        latestPolygon = L.polygon(currentCoordinates).addTo(leafletMap);
+        secondMapCoordinates.push.apply(secondMapCoordinates, currentCoordinates);
+
+        currentCoordinates.push([e.latlng.lat, e.latlng.lng]);
+        L.marker([e.latlng.lat, e.latlng.lng]).addTo(leafletMap);
+
+        // Llamar alerta
+        swal.fire({
+            title: 'Sus Coordenadas:',
+            target: document.getElementById('alert'),
+            html: currentCoordinatesToHTML(),
+            icon: 'info',
+            confirmButtonText: "Gracias!"
+        });
+
+        //SE AGREGA TODO AL SEGUNDO MAPA
+
+        if (leafletMap2 !== undefined) {
+            leafletMap2.off();
+            leafletMap2.remove();
+        }
+        leafletMap2 = L.map("map2").setView(
+            [CENTER_LAT, CENTER_LNG],
+            ZOOM_LEVEL
+        );
+        L.tileLayer(tiles[(Math.floor((Math.random() * 3) + 0))], {
+            maxZoom: 18,
+            attribution:
+                'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+        }).addTo(leafletMap2);
+        for (let i = 0; i < secondMapCoordinates.length; i++) {
+            L.marker([secondMapCoordinates[i][0], secondMapCoordinates[i][1]]).addTo(leafletMap2);
+        }
+
+    }
 }
 
 function onContextMenu(e) {
-  addNewPolygon = true;
+    addNewPolygon = true;
 }
 
 leafletMap.on("click", onMapClick);
 leafletMap.on("contextmenu", onContextMenu);
+
+function currentCoordinatesToHTML() {
+    let html = '';
+    currentCoordinates.forEach((coordenate, index) => {
+        html += `<span><b>${index}:</b></span>${coordenate}</br>`;
+    })
+    return html;
+}
